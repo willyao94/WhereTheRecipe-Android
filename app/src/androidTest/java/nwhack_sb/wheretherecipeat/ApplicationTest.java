b3d471fa4077a4390e5fc17c01ad2805b4cf11ba
Eric Cheng <kingeric0201@hotmail.com>
2015-03-13 22:27:59 -0700

    First commit

---
+++ app/src/androidTest/java/nwhack_sb/wheretherecipeat/ApplicationTest.java
@@ -0,0 +1,13 @@
+package nwhack_sb.wheretherecipeat;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
